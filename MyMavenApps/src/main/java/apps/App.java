e7b6bbfe457c75696bc6566af9c933c602fe445e
linubajy <blinu.1997@gmail.com>
2021-02-14 01:06:13 +0530

    jenkins check

--- MyMavenApps/src/main/java/apps/App.java
+++ MyMavenApps/src/main/java/apps/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("New jenkins build check!");
+        System.out.println("New jenkins build check");
     }
 }
